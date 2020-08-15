7a0dfa891589d84c25089bad0b5b8e1a9808956e
Gugo <gugo1519@gmail.com>
2020-08-15 14:16:56 +0300

    laravel Passport

---
+++ app/Http/Controllers/Auth/ApiAuthController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Str;
+
+class ApiAuthController extends Controller
+{
+    //registracia
+
+    public function register (Request $request) {
+        $validator = Validator::make($request->all(), [
+            'fullName' => 'required|alpha',
+            'phone' => 'required|numeric|digits_between:7,20',
+            'address' => 'required',
+            'passport' =>'required|alpha_num',
+            'site' => 'required',
+            'code' => 'required|numeric|digits_between:8,8',
+            'email' => 'sometimes|required|email',
+            'password' => 'required',
+        ]);
+        if ($validator->fails())
+        {
+            return response(['errors'=>$validator->errors()->all()], 422);
+        }
+        $request['password']=Hash::make($request['password']);
+//        $request['remember_token'] = Str::random(10);
+        $user = User::create($request->toArray());
+        $token = $user->createToken('Laravel Password Grant Client')->accessToken;
+        $response = ['token' => $token];
+        return response($response, 200);
+    }
+
+    //end registracia
+
+    //login
+    public function login (Request $request) {
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|string|email|max:255',
+            'password' => 'required|string|min:6|confirmed',
+        ]);
+        if ($validator->fails())
+        {
+            return response(['errors'=>$validator->errors()->all()], 422);
+        }
+        $user = User::where('email', $request->email)->first();
+        if ($user) {
+            if (Hash::check($request->password, $user->password)) {
+                $token = $user->createToken('Laravel Password Grant Client')->accessToken;
+                $response = ['token' => $token];
+                return response($response, 200);
+            } else {
+                $response = ["message" => "Password mismatch"];
+                return response($response, 422);
+            }
+        } else {
+            $response = ["message" =>'User does not exist'];
+            return response($response, 422);
+        }
+    }
+    //end login
+
+    //logout
+    public function logout (Request $request) {
+        $token = $request->user()->token();
+        $token->revoke();
+        $response = ['message' => 'You have been successfully logged out!'];
+        return response($response, 200);
+    }
+
+//    logout
+}
--- routes/api.php
+++ routes/api.php
@@ -17,8 +17,17 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::group(['middleware' => ['cors', 'json.response']], function () {
+    Route::post('/login', 'Auth\ApiAuthController@login')->name('login.api');
+    Route::post('/register','Auth\ApiAuthController@register')->name('register.api');
+    Route::post('/logout', 'Auth\ApiAuthController@logout')->name('logout.api');
+});
+Route::middleware('auth:api')->group(function () {
+    // our routes to be protected will go in here
+});
+//Route::resource('/login', 'LoginController');
+//Route::resource('/register', 'RegisterController');
+//Route::resource('/admin', 'ProductsController');
+//Route::get('/verify', 'RegisterController@verify');
 
-Route::resource('/login', 'LoginController');
-Route::resource('/register', 'RegisterController');
-Route::resource('/admin', 'ProductsController');
-Route::get('/verify', 'RegisterController@verify');
+
